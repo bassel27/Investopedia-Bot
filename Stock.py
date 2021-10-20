9b11e90f746a19c848f1d44476db41f8d0c50899
unknown <bassel_sabour@hotmail.com>
2021-10-20 18:18:25 +0200

    Revert "Redid whitespace + webdriver setup + scraping returns"
    This reverts commit 135f0fd462b960bd79e1619c5dff938a21018e95.

--- Stock.py
+++ Stock.py
@@ -1,7 +1,8 @@
 from typing import Counter
 from yahoo_fin import stock_info as si
 from tkinter import *
-from tkinter import ttk, messagebox
+from tkinter import ttk
+from tkinter import messagebox
 import threading, functions
 
 import tkinterFunctions 
@@ -9,223 +10,223 @@ from Scraping import *
 from Scrollbar import * 
 
 def organizeOutput(root, n, stock): 
-	counter = 0
-	row = 3
-	column = 0
-	while counter != n:
-		stock[counter].frameOutput(root, row, column, counter, stock)
-		counter += 1
-		if column == 0:
-			column = 1
-		elif column == 1:
-			column =0
-			row += 1
+    counter = 0
+    row = 3
+    column = 0
+    while counter != n:
+        stock[counter].frameOutput(root, row, column, counter, stock)
+        counter += 1
+        if column == 0:
+            column = 1
+        elif column == 1:
+            column =0
+            row += 1
 
 counter = 0
 scraping = Scraping()
 threads = []
 
 class Stock:
-	def takeScreenshot (self):
-		scraping.openWebDriver()
-		scraping.getFinviz(self.ticker)
-		scraping.takeScreenshot()
-		scraping.cropImage()
-		scraping.resizeImage()
-		scraping.quitDriver()
-		self.graph = PhotoImage(file='currentStock.png')	
-		
-	def frameInputStock(self, root, winChance, stock, risk, account):
-		frameInput = LabelFrame(root)
-		frameInput.pack()
-		global counter
-		Label(frameInput, text = functions.returnOrdinal(counter+1) + " stock").grid(row =0, column = 1)
+    def takeScreenshot (self):
+        scraping.openWebDriver()
+        scraping.getFinviz(self.ticker)
+        scraping.takeScreenshot()
+        scraping.cropImage()
+        scraping.resizeImage()
+        scraping.quitDriver()
+        self.graph = PhotoImage(file='currentStock.png')    
+        
+    def frameInputStock(self, root, winChance, stock, risk, account):
+        frameInput = LabelFrame(root)
+        frameInput.pack()
+        global counter
+        Label(frameInput, text = functions.returnOrdinal(counter+1) + " stock").grid(row =0, column = 1)
 
-		Label(frameInput, text = "Enter the ticker").grid(row =1, column =0, sticky = 'w')
-		entryTicker = ttk.Entry(frameInput)
-		entryTicker.grid(row =1, column =1)
+        Label(frameInput, text = "Enter the ticker").grid(row =1, column =0, sticky = 'w')
+        entryTicker = ttk.Entry(frameInput)
+        entryTicker.grid(row =1, column =1)
 
-		Label(frameInput, text = "Enter your target price").grid(row = 2, column =0, sticky = 'w')
-		entryTargetPrice = ttk.Entry(frameInput)
-		entryTargetPrice.grid(row =2, column = 1)
+        Label(frameInput, text = "Enter your target price").grid(row = 2, column =0, sticky = 'w')
+        entryTargetPrice = ttk.Entry(frameInput)
+        entryTargetPrice.grid(row =2, column = 1)
 
-		Label(frameInput, text = "Enter your stop loss price").grid(row = 3, column =0, sticky = 'w')
-		entrystopLoss = ttk.Entry(frameInput)
-		entrystopLoss.grid(row =3, column = 1)
-		
-		wasWinChanceNull = False
-		if(winChance is None):
-			Label(frameInput, text = "Enter win chance").grid(row = 4, column = 0, sticky = 'w')
-			entryWinChance = ttk.Entry(frameInput)
-			entryWinChance.grid(row = 4, column = 1)
-			wasWinChanceNull = True
-	
-		def clickNextStock(isFinished = False):
-			try:
-				#assign to attributes
-				self.ticker = (entryTicker.get()).upper()
-				self.stopLoss = float(entrystopLoss.get())
-				self.targetPrice = float(entryTargetPrice.get())
-				self.currentPrice = float(si.get_live_price(self.ticker))
-				print(type(self.currentPrice))
-				
-				global counter
-					
-				nonlocal winChance
-				if winChance is None:
-					winChance = float(entryWinChance.get())
+        Label(frameInput, text = "Enter your stop loss price").grid(row = 3, column =0, sticky = 'w')
+        entrystopLoss = ttk.Entry(frameInput)
+        entrystopLoss.grid(row =3, column = 1)
+        
+        wasWinChanceNull = False
+        if(winChance is None):
+            Label(frameInput, text = "Enter win chance").grid(row = 4, column = 0, sticky = 'w')
+            entryWinChance = ttk.Entry(frameInput)
+            entryWinChance.grid(row = 4, column = 1)
+            wasWinChanceNull = True
+    
+        def clickNextStock(isFinished = False):
+            try:
+                #assign to attributes
+                self.ticker = (entryTicker.get()).upper()
+                self.stopLoss = float(entrystopLoss.get())
+                self.targetPrice = float(entryTargetPrice.get())
+                self.currentPrice = float(si.get_live_price(self.ticker))
+                print(type(self.currentPrice))
+                
+                global counter
+                    
+                nonlocal winChance
+                if winChance is None:
+                    winChance = float(entryWinChance.get())
 
-				self.winChance = winChance
-				self.lossChance = 1 - self.winChance
-				self.expectancy =100 * abs(((self.targetPrice - self.currentPrice) / self.currentPrice)) * self.winChance - abs(((self.currentPrice - self.stopLoss) / self.currentPrice)) * self.lossChance
-				self.quantity = int((account * (risk / 100)) / abs((self.currentPrice - self.stopLoss)))
-				
-				
-				self.cost = self.currentPrice * self.quantity
-				self.expectedProfit = abs(self.currentPrice - self.targetPrice) * self.quantity
-				if self.currentPrice > self.targetPrice:
-					self.isLong = False
-				else:
-					self.isLong = True
+                self.winChance = winChance
+                self.lossChance = 1 - self.winChance
+                self.expectancy =100 * abs(((self.targetPrice - self.currentPrice) / self.currentPrice)) * self.winChance - abs(((self.currentPrice - self.stopLoss) / self.currentPrice)) * self.lossChance
+                self.quantity = int((account * (risk / 100)) / abs((self.currentPrice - self.stopLoss)))
+                
+                
+                self.cost = self.currentPrice * self.quantity
+                self.expectedProfit = abs(self.currentPrice - self.targetPrice) * self.quantity
+                if self.currentPrice > self.targetPrice:
+                    self.isLong = False
+                else:
+                    self.isLong = True
 
-				if self.isLong == True:
-					self.transaction = "Buy"
-				else:
-					self.transaction = "Sell Short"
-				frameInput.destroy()
-				
-				if(wasWinChanceNull == True):
-					winChance = None
+                if self.isLong == True:
+                    self.transaction = "Buy"
+                else:
+                    self.transaction = "Sell Short"
+                frameInput.destroy()
+                
+                if(wasWinChanceNull == True):
+                    winChance = None
 
-				counter += 1
-				n = counter
+                counter += 1
+                n = counter
 
-				if isFinished == True:
-					global threads
-					stock[counter-1].takeScreenshot()
-					stock.sort(key=lambda x: x.expectancy, reverse=True)
-					Label(root, text = "Results	   ").pack()
-					
-					global mainFrame
-					mainFrame = VerticalScrolledFrame(root, width=1700, borderwidth=2, relief=SUNKEN, background="light gray")
-					mainFrame.pack(fill="both", expand=True)
-					organizeOutput(root, n, stock)
-				else:
-					thread = threading.Thread(target = stock[counter-1].takeScreenshot)
-					thread.start()
-					threads.append(thread)
+                if isFinished == True:
+                    global threads
+                    stock[counter-1].takeScreenshot()
+                    stock.sort(key=lambda x: x.expectancy, reverse=True)
+                    Label(root, text = "Results       ").pack()
+                    
+                    global mainFrame
+                    mainFrame = VerticalScrolledFrame(root, width=1700, borderwidth=2, relief=SUNKEN, background="light gray")
+                    mainFrame.pack(fill="both", expand=True)
+                    organizeOutput(root, n, stock)
+                else:
+                    thread = threading.Thread(target = stock[counter-1].takeScreenshot)
+                    thread.start()
+                    threads.append(thread)
 
-					stock.append(Stock())
-					stock[counter].frameInputStock(root, winChance, stock, risk, account)
-			except:
-				if wasWinChanceNull == True:
-					winChance = None
-				messagebox.showerror("Error", "Wrong entry. Please try again!")
-				frameInput.destroy()
-				stock[counter].frameInputStock(root, winChance, stock, risk, account)
-			
-		def checkEntry(event):
-			nonlocal wasWinChanceNull
-			if wasWinChanceNull == True:
-				if entrystopLoss.get() and entryTargetPrice.get() and entryWinChance.get():
-					buttonNextStock.config(state=NORMAL)
-					buttonFinish.config(state=NORMAL)
-				else:
-					buttonNextStock.config(state=DISABLED)
-					buttonFinish.config(state=DISABLED)
-			else:
-				if entrystopLoss.get() and entryTargetPrice.get():
-					buttonNextStock.config(state=NORMAL)
-					buttonFinish.config(state=NORMAL)
-				else:
-					buttonNextStock.config(state=DISABLED)
-					buttonFinish.config(state=DISABLED)
-			
-		entryTargetPrice.bind('<KeyRelease>',checkEntry)		 
-		entrystopLoss.bind('<KeyRelease>', checkEntry)
-		if wasWinChanceNull == True:
-			entryWinChance.bind('<KeyRelease>', checkEntry)  
+                    stock.append(Stock())
+                    stock[counter].frameInputStock(root, winChance, stock, risk, account)
+            except:
+                if wasWinChanceNull == True:
+                    winChance = None
+                messagebox.showerror("Error", "Wrong entry. Please try again!")
+                frameInput.destroy()
+                stock[counter].frameInputStock(root, winChance, stock, risk, account)
+            
+        def checkEntry(event):
+            nonlocal wasWinChanceNull
+            if wasWinChanceNull == True:
+                if entrystopLoss.get() and entryTargetPrice.get() and entryWinChance.get():
+                    buttonNextStock.config(state=NORMAL)
+                    buttonFinish.config(state=NORMAL)
+                else:
+                    buttonNextStock.config(state=DISABLED)
+                    buttonFinish.config(state=DISABLED)
+            else:
+                if entrystopLoss.get() and entryTargetPrice.get():
+                    buttonNextStock.config(state=NORMAL)
+                    buttonFinish.config(state=NORMAL)
+                else:
+                    buttonNextStock.config(state=DISABLED)
+                    buttonFinish.config(state=DISABLED)
+            
+        entryTargetPrice.bind('<KeyRelease>',checkEntry)         
+        entrystopLoss.bind('<KeyRelease>', checkEntry)
+        if wasWinChanceNull == True:
+            entryWinChance.bind('<KeyRelease>', checkEntry)  
 
-		buttonNextStock = ttk.Button(frameInput, text = "Onto the next stock", command = clickNextStock, state = DISABLED)
-		buttonNextStock.grid(row = 5, column =1)
-		buttonFinish = ttk.Button(frameInput, text = "Finish", command = lambda: clickNextStock(isFinished = True), state=DISABLED)
-		buttonFinish.grid(row = 6, column =1)
-		
-	def frameOutput(self, root, rowNo, columnNo, counter, stock):
-		def setEntryLimitPrice (entryLimitPrice):
-			entryLimitPrice.delete(0, 'end')
-			entryLimitPrice.grid(row = 2, column =0)
-			if self.isLong == True:
-				entryLimitPrice.insert(0, round(self.currentPrice + 0.02, 2))
-			else:
-				entryLimitPrice.insert(0, round(self.currentPrice - 0.02, 2))
+        buttonNextStock = ttk.Button(frameInput, text = "Onto the next stock", command = clickNextStock, state = DISABLED)
+        buttonNextStock.grid(row = 5, column =1)
+        buttonFinish = ttk.Button(frameInput, text = "Finish", command = lambda: clickNextStock(isFinished = True), state=DISABLED)
+        buttonFinish.grid(row = 6, column =1)
+        
+    def frameOutput(self, root, rowNo, columnNo, counter, stock):
+        def setEntryLimitPrice (entryLimitPrice):
+            entryLimitPrice.delete(0, 'end')
+            entryLimitPrice.grid(row = 2, column =0)
+            if self.isLong == True:
+                entryLimitPrice.insert(0, round(self.currentPrice + 0.02, 2))
+            else:
+                entryLimitPrice.insert(0, round(self.currentPrice - 0.02, 2))
 
-		frameOutput = LabelFrame(mainFrame) 
-		frameOutput.grid(row = rowNo, column = columnNo)
-		Label(frameOutput, text = str(counter + 1) + ". " + self.ticker).grid(row = 0, column = 0, sticky = 'w')
-		Label(frameOutput, text = f"Expectancy: {self.expectancy:.2f}%").grid(row = 1, column = 0, sticky = 'w')
-		
-		Label(frameOutput, text = "						 order: ").grid(row = 2, column = 0, sticky = 'w' )
-		entryLimitPrice = ttk.Entry(frameOutput)
-		setEntryLimitPrice(entryLimitPrice)
-		var = StringVar()
-		var.set("Limit")
-		options = ["Limit", "Market"]
-		orderTypeDropDown = ttk.OptionMenu(frameOutput, var, options[0], *options )
-		orderTypeDropDown.grid(row = 2, column = 0, sticky = 'w')  
-			  
-		def dropDownFunction(*args):
-			if var.get() == options[0]:
-				setEntryLimitPrice(entryLimitPrice)		
-			else:
-				entryLimitPrice.delete(0, 'end')
-				entryLimitPrice.grid_forget()
-		var.trace('w', dropDownFunction)
+        frameOutput = LabelFrame(mainFrame) 
+        frameOutput.grid(row = rowNo, column = columnNo)
+        Label(frameOutput, text = str(counter + 1) + ". " + self.ticker).grid(row = 0, column = 0, sticky = 'w')
+        Label(frameOutput, text = f"Expectancy: {self.expectancy:.2f}%").grid(row = 1, column = 0, sticky = 'w')
+        
+        Label(frameOutput, text = "                         order: ").grid(row = 2, column = 0, sticky = 'w' )
+        entryLimitPrice = ttk.Entry(frameOutput)
+        setEntryLimitPrice(entryLimitPrice)
+        var = StringVar()
+        var.set("Limit")
+        options = ["Limit", "Market"]
+        orderTypeDropDown = ttk.OptionMenu(frameOutput, var, options[0], *options )
+        orderTypeDropDown.grid(row = 2, column = 0, sticky = 'w')  
+              
+        def dropDownFunction(*args):
+            if var.get() == options[0]:
+                setEntryLimitPrice(entryLimitPrice)        
+            else:
+                entryLimitPrice.delete(0, 'end')
+                entryLimitPrice.grid_forget()
+        var.trace('w', dropDownFunction)
 
-		Label(frameOutput, text = "Stop loss: " + str(self.stopLoss) + " $").grid(row = 3, column = 0, sticky = 'w')
-		Label(frameOutput, text = "Expected profit: " + str(round(self.expectedProfit, 2))+ " $").grid(row = 4, column =0, sticky = 'w')
-		
-		if self.isLong == True:
-			labelSharesLonged = Label(frameOutput, text = "Shares longed: " + str(self.quantity))
-			labelSharesLonged.grid(row = 5, column = 0, sticky = 'w')
-			labelCost= Label(frameOutput, text = "Total cost: " + str(round(self.cost, 2)) + " $")
-			labelCost.grid(row = 6, column = 0, sticky = 'w')
-		else:
-			labelSharesShorted = Label(frameOutput, text = "Shares shorted: " + str(self.quantity))
-			labelSharesShorted.grid(row = 5, column = 0, sticky = 'w')
-			Label(frameOutput, text = ' ').grid(row = 6, column = 0)
-		
-		screenshotLabel = Label(frameOutput, image = self.graph)
-		screenshotLabel.img = self.graph
-		screenshotLabel.grid(row = 7, column = 0)
+        Label(frameOutput, text = "Stop loss: " + str(self.stopLoss) + " $").grid(row = 3, column = 0, sticky = 'w')
+        Label(frameOutput, text = "Expected profit: " + str(round(self.expectedProfit, 2))+ " $").grid(row = 4, column =0, sticky = 'w')
+        
+        if self.isLong == True:
+            labelSharesLonged = Label(frameOutput, text = "Shares longed: " + str(self.quantity))
+            labelSharesLonged.grid(row = 5, column = 0, sticky = 'w')
+            labelCost= Label(frameOutput, text = "Total cost: " + str(round(self.cost, 2)) + " $")
+            labelCost.grid(row = 6, column = 0, sticky = 'w')
+        else:
+            labelSharesShorted = Label(frameOutput, text = "Shares shorted: " + str(self.quantity))
+            labelSharesShorted.grid(row = 5, column = 0, sticky = 'w')
+            Label(frameOutput, text = ' ').grid(row = 6, column = 0)
+        
+        screenshotLabel = Label(frameOutput, image = self.graph)
+        screenshotLabel.img = self.graph
+        screenshotLabel.grid(row = 7, column = 0)
 
-		def isCheckButtonChecked():
-			if checkButtonExecuteVar.get():
-				orderType = var.get()
-				try:
-					limitPrice = entryLimitPrice.get()
-				except:
-					limitPrice = 0
+        def isCheckButtonChecked():
+            if checkButtonExecuteVar.get():
+                orderType = var.get()
+                try:
+                    limitPrice = entryLimitPrice.get()
+                except:
+                    limitPrice = 0
 
-				scraping.openWebDriver()
-				scraping.login()
-				maxQuantity = scraping.executeOrder(self.ticker, self.transaction, self.quantity, orderType, limitPrice)
-				scraping.quitDriver()
+                scraping.openWebDriver()
+                scraping.login()
+                maxQuantity = scraping.executeOrder(self.ticker, self.transaction, self.quantity, orderType, limitPrice)
+                scraping.quitDriver()
 
-				if self.quantity > maxQuantity:
-					self.quantity = maxQuantity
-					self.cost = self.currentPrice * self.quantity
+                if self.quantity > maxQuantity:
+                    self.quantity = maxQuantity
+                    self.cost = self.currentPrice * self.quantity
 
-					if self.isLong == True:
-						labelCost.destroy()
-						labelSharesLonged.destroy()
-						Label(frameOutput, text = "Shares longed: " + str(self.quantity) + "  (The number of shares was exceeding Investopedia's maximum for this stock)").grid(row = 5, column = 0, sticky = 'w')
-						Label(frameOutput, text = "Total cost: " + str(self.cost) + " $").grid(row = 6, column = 0, sticky = 'w')
-					else:
-						labelSharesShorted.destroy()
-						Label(frameOutput, text = "Shares shorted: " + str(self.quantity)).grid(row = 5, column = 0, sticky = 'w')
+                    if self.isLong == True:
+                        labelCost.destroy()
+                        labelSharesLonged.destroy()
+                        Label(frameOutput, text = "Shares longed: " + str(self.quantity) + "  (The number of shares was exceeding Investopedia's maximum for this stock)").grid(row = 5, column = 0, sticky = 'w')
+                        Label(frameOutput, text = "Total cost: " + str(self.cost) + " $").grid(row = 6, column = 0, sticky = 'w')
+                    else:
+                        labelSharesShorted.destroy()
+                        Label(frameOutput, text = "Shares shorted: " + str(self.quantity)).grid(row = 5, column = 0, sticky = 'w')
 
-		Label(frameOutput, text = "Execute trade?").grid(row = 8, column = 0, sticky = 'w')
-		checkButtonExecuteVar = IntVar()
-		checkButtonExecute = ttk.Checkbutton(frameOutput, variable = checkButtonExecuteVar, command = isCheckButtonChecked)
-		checkButtonExecute.grid(row = 8, column = 1)
+        Label(frameOutput, text = "Execute trade?").grid(row = 8, column = 0, sticky = 'w')
+        checkButtonExecuteVar = IntVar()
+        checkButtonExecute = ttk.Checkbutton(frameOutput, variable = checkButtonExecuteVar, command = isCheckButtonChecked)
+        checkButtonExecute.grid(row = 8, column = 1)
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from tkinter import *
 from tkinterFunctions import *
 from Stock import *
 
-root = Tk() #creating the root widget which the empty window
+root = Tk()     #creating the root widget which the empty window
 
 atExit(root)
-frameInputAll(root)
+frameInputAll (root)
 
 frameAccAndCash(root)
 
-root.mainloop() #program loop
+root.mainloop()     #program loop
